4e29190bd2196bdf7f31c1060608da45a264d753
Munira <muniraqazi98@gmail.com>
2022-04-17 19:05:12 -0400

    responsiveness testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import "./styles/styles.scss";
+
+
+
 import BookShelves from "./components/BookShelves";
 
 
+
 function App() {
+  const [coverID, setCoverID] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [books, setBooks] = useState([]);
+  const [searchedTitle, setSearchedTitle] = useState("");
+
+  useEffect(() => {
+    axios.get(`http://openlibrary.org/search.json?q=${userInput}`,)
+      .then (response => {
+        setBooks(response.data.docs); 
+      })
+    }, [userInput]);
+
+    const handleChange = (event) => {
+      setSearchedTitle(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      setUserInput(searchedTitle);
+    }
+    console.log(userInput)
+
+
   return (
-    <div>
-  <BookShelves/>
+    <div className='wrapper'>
+      <div className="mainContainer">
+        <div className="headerContainer">
+          <h1>✿ Little Library ✿</h1>
+          <h2>🕮 Search for any book title:</h2>
+          <form onSubmit={handleSubmit}>
+            <input onChange={handleChange} type="text" placeholder="Book Title..."/>
+            <button>🔍</button>
+          </form>
+
+          
+
+        </div> {/**end of header container */}
+        <div className="shelfContainer">
+          <BookShelves 
+            books={books}
+            setBooks={setBooks}
+          />
+        </div>
+      </div> {/**end of main container */}
     </div>
   );
 }
 
 export default App;
+
+
+
+
--- src/components/BookShelves.jsx
+++ src/components/BookShelves.jsx
@@ -1,8 +1,97 @@
-import React from 'react'
+import React, {useState} from 'react'
+import noImage from '../assets/no-image-available.webp'
 
-const BookShelves = () => {
+import { AiOutlineSortAscending } from 'react-icons/ai';
+import { AiOutlineSortDescending } from 'react-icons/ai';
+
+import { BsSortNumericDown } from 'react-icons/bs';
+import { BsSortNumericUp } from 'react-icons/bs';
+
+const BookShelves = ({books, setBooks}) => {
+  const coverImage = (coverID) => {
+    return `http://covers.openlibrary.org/b/id/${coverID}-M.jpg`;
+  };
+
+  const openLibraryUrl = "https://openlibrary.org";
+
+  const [sortAlphabetically, setSortAlphabetically] = useState(false);
+  const [sortByYear, setSortByYear] = useState(false);
+
+  const sortByTitle = () => {
+    const sortedResult = [...books];
+    const sortedList = sortedResult.sort((a, b) =>
+    sortAlphabetically
+        ? b.title.localeCompare(a.title)
+        : a.title.localeCompare(b.title)
+    );
+    setBooks(sortedList);
+    setSortAlphabetically(!sortAlphabetically);
+  };
+
+  const sortByPublishedYear = () => {
+    const sortedResult = [...books];
+    const sortedList = sortedResult.sort((a, b) =>
+      sortByYear
+        ? a.first_publish_year - b.first_publish_year
+        : b.first_publish_year - a.first_publish_year
+    );
+    setBooks(sortedList);
+    setSortByYear(!sortByYear);
+  };
+
   return (
-    <div>BookShelves</div>
+    <>
+      <div className='buttonContainer'>
+        <div className='sortingButton' onClick={sortByTitle}>
+        {sortAlphabetically ?
+          <AiOutlineSortAscending/>
+          :
+          <AiOutlineSortDescending/>
+        }
+        </div>
+        <div className='sortingButton' onClick={sortByPublishedYear}>
+        {sortByYear ?
+          <BsSortNumericDown/>
+          :
+          <BsSortNumericUp/>
+        }
+        </div>
+      </div>
+
+      <div className='bookContainer'>
+        {books.map((data) => (
+          <div className='shelf'>
+            <div className='bookImg'>
+              { data.cover_i ?
+                <img src={coverImage(data.cover_i)} alt="" />
+                :
+                <img src={noImage} alt="" />
+              }
+            </div>
+            <div className='bookInfo'>
+              <p>{data.title}</p>
+
+              <div className='authContainer'>
+                {data.author_name ? 
+                  data.author_name.map((author, index) => (
+                    <p key={index}>{author}</p> 
+                  ))
+                  :
+                  <p>N/A</p>
+                }
+              </div>
+
+              <p>Published: {data.first_publish_year ? 
+                data.first_publish_year : "N/A"}
+              </p>
+              <div className='moreButtonContainer'>
+                <a href={openLibraryUrl + data.key}>More Info</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
